933afbae7d7c449648a17b0b36607801d168ca13
marlens123 <marlena@itdex.de>
2025-03-20 10:07:05 -0400

    debugging

--- src/train.py
+++ src/train.py
@@ -55,11 +55,14 @@ model = model.to(device)
 
 optimizer = torch.optim.SGD(model.parameters(), lr=0.01, momentum=0.9, weight_decay=0.0001)
 
+valid = []
 invalid = []
 for data, target in train_dataloader:
     invalid.append(target['cls'] == -1)
+    valid.append(target['cls'] != -1)
 
 print("Invalid data = ", sum(invalid))
+print("Valid data = ", sum(valid))
 
 # Training loop.
 for epoch in range(NUM_EPOCHS):
