59bb119335ac94005ba28056770397a8d7d6b0ed
marlens123 <marlena@itdex.de>
2025-04-24 17:11:39 -0400

    rm wandb

--- run_s2_swinb_adamw_lr0.0005.sh
+++ run_s2_swinb_adamw_lr0.0005.sh
@@ -9,6 +9,4 @@ module load anaconda/3
 
 conda activate rs_fairness
 
-export WANDB_API_KEY=d2ca547c9f807e8db70308537f4d7b64b6077b81
-
 python -m src.train --pretraining_dataset "Satlas" --satlas_model_identifier "Sentinel2_SwinB_SI_RGB" --config_file "adamw_lr0.0005.py"
